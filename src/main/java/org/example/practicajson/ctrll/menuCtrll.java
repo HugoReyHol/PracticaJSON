1131861ee34977820d9eb3807bec104fabb32f42
Hugo <hugo.reyhol@educa.jcyl.es>
2024-10-07 11:08:02 +0200

    Proyecto acabado

--- src/main/java/org/example/practicajson/ctrll/menuCtrll.java
+++ src/main/java/org/example/practicajson/ctrll/menuCtrll.java
@@ -1,14 +1,49 @@
 package org.example.practicajson.ctrll;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import org.example.practicajson.model.Pelicula;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
 
 public class menuCtrll {
     @FXML
-    private Label welcomeText;
+    private TextField salidaTitulo, salidaFecha, salidaGenero, salidaDirector;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ListView<Pelicula> listaPeliculas;
+
+    private final ObjectMapper JSON_MAPPER = new ObjectMapper();
+
+    private ObservableList<Pelicula> peliculas = FXCollections.observableArrayList();
+
+
+    public void onImportar(ActionEvent actionEvent) throws IOException {
+        ArrayList<Pelicula> p = JSON_MAPPER.readValue(new File("src/main/resources/json/peliculas.json"),
+                JSON_MAPPER.getTypeFactory().constructCollectionType(ArrayList.class, Pelicula.class));
+
+        peliculas.addAll(p);
+
+        listaPeliculas.setItems(peliculas);
+
+    }
+
+
+    public void onClick(MouseEvent mouseEvent) {
+        Pelicula p = listaPeliculas.getSelectionModel().getSelectedItem();
+
+        salidaDirector.setText(p.getDirector());
+        salidaFecha.setText(p.getFecha());
+        salidaGenero.setText(p.getGenero());
+        salidaTitulo.setText(p.getTitulo());
+
     }
 }
